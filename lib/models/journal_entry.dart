34cf1ae27bc8e1c844d37f98f318fedf7c6e7f2e
Mohab Namira <namiramohab@gmail.com>
2026-09-17 03:22:14 +0300

    feat: add JournalEntry model with Hive support

---
+++ lib/models/journal_entry.dart
@@ -0,0 +1,24 @@
+import 'package:hive/hive.dart';
+part 'journal_entry.g.dart';
+
+@HiveType(typeId: 0)
+class JournalEntry extends HiveObject {
+  @HiveField(0)
+  String title;
+
+  @HiveField(1)
+  String content;
+
+  @HiveField(2)
+  DateTime date;
+
+  @HiveField(3)
+  String? mood;
+
+  JournalEntry({
+    required this.title,
+    required this.content,
+    required this.date,
+    this.mood,
+  });
+}
---
+++ lib/models/journal_entry.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'journal_entry.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class JournalEntryAdapter extends TypeAdapter<JournalEntry> {
+  @override
+  final int typeId = 0;
+
+  @override
+  JournalEntry read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return JournalEntry(
+      title: fields[0] as String,
+      content: fields[1] as String,
+      date: fields[2] as DateTime,
+      mood: fields[3] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, JournalEntry obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.content)
+      ..writeByte(2)
+      ..write(obj.date)
+      ..writeByte(3)
+      ..write(obj.mood);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is JournalEntryAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
